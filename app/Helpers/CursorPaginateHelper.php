522c845ed0622e7a51b47cea9a23944b2e6a0db1
Afif-20 <fadlurrahman.afif23@gmail.com>
2025-08-05 09:22:13 +0700

    menambahkan isi file CursorPagnitionHelper

--- app/Helpers/CursorPaginateHelper.php
+++ app/Helpers/CursorPaginateHelper.php
@@ -2,13 +2,99 @@
 
 namespace App\Helpers;
 
+use Illuminate\Pagination\Cursor;
+use Illuminate\Pagination\CursorPaginator;
+
 class CursorPaginateHelper
 {
     /**
-     * Create a new class instance.
+     * Get to paginate array.
+     *
+     * @param CursorPaginator $paginator
+     * @return array
      */
-    public function __construct()
+
+    public static function getPaginate(CursorPaginator $paginator): array
     {
-        //
+        return [
+            'path' => self::path($paginator),
+            'per_page' => self::perPage($paginator),
+            'next_cursor' => self::nextCursor($paginator),
+            'next_page_url' => self::nextPageUrl($paginator),
+            'prev_cursor' => self::previousCursor($paginator),
+            'prev_page_url' => self::previousPageUrl($paginator),
+        ];
+    }
+
+    /**
+     * Get the current path.
+     *
+     * @param CursorPaginator $paginator
+     * @return string
+     */
+
+    public static function path(CursorPaginator $paginator): string
+    {
+        return $paginator->path();
+    }
+
+    /**
+     * Get the current per page.
+     *
+     * @param CursorPaginator $paginator
+     * @return int
+     */
+
+    public static function perPage(CursorPaginator $paginator): int
+    {
+        return $paginator->perPage();
+    }
+
+    /**
+     * Get the next cursor.
+     *
+     * @param CursorPaginator $paginator
+     * @return string|null
+     */
+    public static function nextCursor(CursorPaginator $paginator): string|null
+    {
+        return $paginator->hasMorePages() ? $paginator->nextCursor()->encode() : null;
+    }
+
+    /**
+     * Get the next page URL.
+     *
+     * @param CursorPaginator $paginator
+     * @return string|null
+     */
+    public static function nextPageUrl(CursorPaginator $paginator): string|null
+    {
+        return $paginator->hasMorePages() ? $paginator->nextPageUrl() : null;
+    }
+
+    /**
+     * Get the previous cursor.
+     *
+     * @param CursorPaginator $paginator
+     * @return string|null
+     */
+    public static function previousCursor(CursorPaginator $paginator): string|null
+    {
+        // Ambil cursor sebelumnya
+        $prevCursor = $paginator->previousCursor();
+
+        // Jika ada: encode, jika tidak: null
+        return $prevCursor ? $prevCursor->encode() : null;
+    }
+
+    /**
+     * Get the previous page URL.
+     *
+     * @param CursorPaginator $paginator
+     * @return string|null
+     */
+    public static function previousPageUrl(CursorPaginator $paginator): string|null
+    {
+        return $paginator->hasMorePages() ? $paginator->previousPageUrl() : null;
     }
 }
